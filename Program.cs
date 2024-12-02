60e1a3a76309748b88202f1832fca2fe1b545b0d
Frank Riccardi <dd3637110@gmail.com>
2024-12-02 21:42:53 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloDotNetProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Frank");
+        }
+    }
+}
